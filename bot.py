6a493f589abedc5e053c288b1353947406687b86
Savithu_s3 <savithusapumal@gmail.com>
2023-03-04 21:29:07 +0530

    config

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
-#!
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I'm a bot!")
+
+updater = Updater(token='5938743745:AAH_vYTPnMWsol1fbtM_Ae5QOAe9Em-_qBw', use_context=True)
+start_handler = CommandHandler('start', start)
+updater.dispatcher.add_handler(start_handler)
+updater.start_polling()
